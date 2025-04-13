a187f38f52842ae2b946c50e24cae96fbbb538dc
deepak1640304 <dk1640304@gmail.com>
2025-04-13 00:20:19 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -26,3 +26,25 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y 
 sudo apt-get install jenkins -y
+
+
+------------------------------------------
+# CentOs steps:
+sudo yum update
+sudo yum install java-devel -y
+java --version
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo nano /etc/yum.repos.d/jenkins.repo
+[jenkins]
+name=Jenkins-stable
+baseurl=http://pkg.jenkins.io/redhat
+gpgcheck=1
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
+sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+sudo firewall-cmd --reload
+
+
